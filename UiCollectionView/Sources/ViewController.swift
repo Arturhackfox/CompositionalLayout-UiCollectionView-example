270d66850b2403db3a17617c3666874bb2200b2b
Arthur Sh <arthur.shemshur@gmail.com>
2024-02-01 09:20:13 +0200

    - project prepare

---
+++ UiCollectionView/Sources/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  UiCollectionView
+//
+//  Created by Arthur Sh on 01.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // MARK: - Ui
+    
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: - Setup
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupLayout() {
+        
+    }
+    
+    // MARK: - Actions
+
+
+}
+
